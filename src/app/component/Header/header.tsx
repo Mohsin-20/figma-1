b2d0c939506e2e89ef02b3fd452ca884b0bc6965
Mohsin-20 <mohsinlakhani129@gmail.com>
2024-11-10 00:39:57 +0500

    first commit

---
+++ src/app/component/Header/header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header: React.FC = () => {
+    return (
+      <header className = "  bg-[#A29875] h-[134px] top-5 left-9">
+        <h1 className=" text-white font-[Rye] font-[700] text-[75px] leading-[93.75px] p-5 ">MANZARRI</h1>
+      </header>
+    );
+  };
+  
+  export default Header;
+  5;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,41 @@
 import Image from "next/image";
+import Header from "./component/Header/header";
+import pictire from "../../public/image/1.jpeg";
+
+
+
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    
+   <div className ='h-screen'>
+    <Header/>
+    <div className = "flex h-[80%]"> 
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  <div className="w-1/2 flex flex-col justify-center items-start m-12 ">
+    <h1 className="font-[Rye] font-bold text-[40px] w-[496px] leading-tight">
+      <b>IMPECCABLE CRAFTSMANSHIP AND FINESSE</b>
+    </h1>
+    <p className="text-[30px] w-[902px]  font-[Libre Bodoni] font-[300] text-[#787054]  me-80  space-y-3 mt-9 mb-9">
+      An example of intricate workmanship and detail, elegant necklaces and long and short chains form a part of our desirable collection.
+    </p>
+    <button className=" bg-[#A29875] w-[247.67px] h-[49.2px] t-[710.32px] p-[8.6px] r-[8.6px] rounded text-white font-[Rye] font-medium text-2xl"
+>Explore Now</button>
     </div>
+
+  <div className="w-1/2 flex justify-center item-center">
+  <div className="flex-shrink-0 border border-white border-solid ml-18 w-[380px] h-[525.43px]rounded-[128.99px] ">
+    <Image 
+      src={pictire} 
+      alt="this is prc" 
+      width={490} 
+      height={667} 
+      className="w-[421.38px] h-[573.59px] rounded-br-[107px] rounded-tl-[107px] object-cover"
+    />
+  </div>
+</div>
+</div>
+
+    </div> 
   );
 }
